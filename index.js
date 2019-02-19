3f620cf921c4a2824e27a1f220e83caa54650171
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-02-19 22:08:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ function theBeatlesPlay(players, instruments) {
 }
 return allPlayers
 }
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
 }
 function iLoveTheBeatles(num) {
     var love = [];
